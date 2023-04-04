7aa5574d01e0f2f4e0b038b4a4bcbd531470b329
roylee0912 <leeroy0912@gmail.com>
2023-04-04 16:12:09 -0400

    user auth completed, testing deploy before continuing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
+  # root "articles#index"
+  resources :users, only: [:create, :show, :index]
+
+  post '/users',         to: 'users#create'
+  get '/users/:user_id', to: 'users#show'
+  get '/users',          to: 'users#index'
+
+  post '/login',    to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
+
 end
