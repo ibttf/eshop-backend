affa1ff0e0df8bc16e0e20b6fd85f9e402dbeaf9
roylee0912 <leeroy0912@gmail.com>
2023-05-23 02:01:54 -0400

    took out email from migration, fixed login

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_042852) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
