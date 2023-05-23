65cdccdeffe1c47620c10571ed73a2001550649f
roylee0912 <leeroy0912@gmail.com>
2023-05-23 16:08:27 -0400

    reverted back, seemed to break

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_controller.session = { :domain => ".netlify.app" }
+  # config.action_controller.session = { :domain => ".netlify.app" }
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
